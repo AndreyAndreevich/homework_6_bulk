e70fafe2d329daa25016b6a7da46c21d32e2c06c
AndreyAndreevich <l.a.r.p@yandex.ru>
2018-05-26 22:30:55 +0300

    fix command

--- main.cpp
+++ main.cpp
@@ -2,28 +2,23 @@
 
 #include "Writers.h"
 
-int main() 
+int main(int argc, char *argv[]) 
 {
-
   try {
-    int N;
-    std::cin >> N;
-
     auto handler = std::make_shared<Handler>();
     auto consoleWriter = std::make_shared<ConsoleWriter>();
     auto fileWriter = std::make_shared<FileWriter>();
     consoleWriter->subscribe(handler);
     fileWriter->subscribe(handler);
 
-    handler->setN(N);
-    
+    handler->setN(std::atoi(argv[1]));   
     std::string line;
     while (std::getline(std::cin, line)) {
       handler->addCommand(line);
     }
     handler->stop();
   } catch(const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+    std::cerr << e.what() << std::endl;
   }
 
   return 0;
